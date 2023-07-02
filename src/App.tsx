6372ff10d7196b335a75b64b57d5f9b6115b42c0
bhubany <yadav.bhuban.by@gmail.com>
2023-07-02 19:08:12 +0545

    fix(): separate list

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 
 function App(): JSX.Element {
   return (
-    <ScrollView>
-      <View>
-        <Text
-          style={{
-            fontSize: 30,
-            display: 'flex',
-            textAlign: 'center',
-            paddingTop: 10,
-            fontWeight: 'bold',
-          }}>
-          Hello From React Native
-        </Text>
+    <View style={styles.container}>
+      <View style={styles.subContainer}>
+        <Text>Hello From React Native</Text>
       </View>
-    </ScrollView>
+      <ScrollView>
+        <Text>From Scroll View</Text>
+      </ScrollView>
+    </View>
   );
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: 'green',
+    borderWidth: 5,
+    margin: 5,
+    padding: 5,
+    height: 'auto',
+    width: 'auto',
+  },
+  subContainer: {
+    fontSize: 30,
+    display: 'flex',
+    textAlign: 'center',
+    paddingTop: 10,
+    fontWeight: 'bold',
+    borderColor: 'green',
+  },
+});
