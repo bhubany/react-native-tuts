d8517372a0d0872bbccaed22ca2be2bba4d3aa35
bhubany <yadav.bhuban.by@gmail.com>
2023-07-02 21:18:09 +0545

    feat(): components in loop with flatlist

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,58 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
 
 function App(): JSX.Element {
+  const users = [
+    {
+      id: 1,
+      name: 'abc',
+      email: 'abc@gmail.com',
+    },
+    {
+      id: 2,
+      name: 'def',
+      email: 'def@gmail.com',
+    },
+    {
+      id: 3,
+      name: 'ghi',
+      email: 'ghi@gmail.com',
+    },
+    {
+      id: 4,
+      name: 'jkl',
+      email: 'jkl@gmail.com',
+    },
+    {
+      id: 5,
+      name: 'mno',
+      email: 'mno@gmail.com',
+    },
+  ];
   return (
     <View style={styles.container}>
       <View style={styles.subContainer}>
         <Text>Hello From React Native</Text>
       </View>
       <ScrollView>
-        <Text>From Scroll View</Text>
+        <FlatList
+          data={users}
+          renderItem={({item}) => <UserData item={item} />}
+        />
       </ScrollView>
     </View>
   );
 }
+
+const UserData = props => {
+  const item = props.item;
+  return (
+    <View key={item.id} style={styles.box}>
+      <Text style={styles.item}> {item.name}</Text>
+      <Text style={styles.item}> {item.email}</Text>
+    </View>
+  );
+};
 
 export default App;
 
@@ -32,5 +72,18 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     fontWeight: 'bold',
     borderColor: 'green',
+  },
+  item: {
+    fontSize: 24,
+    color: 'orange',
+    flex: 1,
+    margin: 2,
+    textAlign: 'center',
+  },
+  box: {
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderColor: 'orange',
+    marginBottom: 10,
   },
 });
